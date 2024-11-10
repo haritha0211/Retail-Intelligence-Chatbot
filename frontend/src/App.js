18690326120bc43210c8cfd2fa8b3f24b6040422
chandan-yadav0011 <chandanyadav8145@gmail.com>
2024-11-10 19:05:56 +0530

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from './Components/Login';
+import Signup from './Components/Signup';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <LoginPage/>
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
